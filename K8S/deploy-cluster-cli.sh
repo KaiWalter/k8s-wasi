48738200a6b633915ed29341e4394a204a680546
Kai Walter <kai.walter@outlook.de>
2023-04-29 17:38:47 +0000

    try with AKS

--- K8S/deploy-cluster-cli.sh
+++ K8S/deploy-cluster-cli.sh
@@ -1,16 +1,18 @@
 #!/bin/sh
+# https://learn.microsoft.com/en-us/azure/aks/use-wasi-node-pools
+
 SUBSCRIPTION_ID=$(az account show --query id -o tsv)
 TENANT_ID=$(az account show --query tenantId -o tsv)
-RESOURCE_PREFIX="k8s-wasi"
+RESOURCE_PREFIX="kw-k8s-wasi"
 RESOURCE_GROUP="$RESOURCE_PREFIX-rg"
 CLUSTER_NAME="$RESOURCE_PREFIX"
-LOCATION="westeurope"
+LOCATION="northeurope"
 
 az group create --name $RESOURCE_GROUP --location $LOCATION
 
 az aks create -g $RESOURCE_GROUP -n $CLUSTER_NAME \
-    --enable-managed-identity
-    --node-count 1
+    --enable-managed-identity \
+    --node-count 3 \
     --node-vm-size Standard_B2s \
     --node-osdisk-size 32 \
      --enable-addons monitoring --generate-ssh-keys
@@ -22,6 +24,8 @@ az aks nodepool add \
     --cluster-name $CLUSTER_NAME \
     --name wasi \
     --node-count 1 \
+    --labels spin-enabled=true slight-enabled=true \
+    --node-vm-size Standard_B2s \
     --workload-runtime wasmwasi 
 
 az aks nodepool show -g $RESOURCE_GROUP \
